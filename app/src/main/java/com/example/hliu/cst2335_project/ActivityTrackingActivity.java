d6ea78f7cf4d3ae44ea0e31b715a5d99156a7358
cliff <cliff_ok@outlook.com>
2017-11-16 22:18:47 -0500

    Geyan commit

--- app/src/main/java/com/example/hliu/cst2335_project/ActivityTrackingActivity.java
+++ app/src/main/java/com/example/hliu/cst2335_project/ActivityTrackingActivity.java
@@ -3,24 +3,106 @@ package com.example.hliu.cst2335_project;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class ActivityTrackingActivity extends Activity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.example.hliu.cst2335_project.ActivityTrackingPkg.ActivityMenuHelp;
+import com.example.hliu.cst2335_project.ActivityTrackingPkg.FragmentNavBot;
+
+public class ActivityTrackingActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tracking);
+        setContentView(R.layout.activity_home);
 
-//        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigationBar_help_h);
-//        bottomNavigationView.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
-//
-//            @Override
-//            public void onNavigationItemReselected(@NonNull MenuItem item) {
-//                Toast.makeText(getApplicationContext(), "Navigation bar Clicked!",
-//                        Toast.LENGTH_SHORT).show();
-//
-//            }
-//        });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentNavBot fragment = new FragmentNavBot();
+        android.support.v4.app.FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_help) {
+            Intent intent = new Intent(ActivityTrackingActivity.this, ActivityMenuHelp.class);
+            startActivity(intent);
+
+            return true;
+        }
+
+        if (id == R.id.action_quit) {
+            finishAffinity();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_activity) {
+            FragmentNavBot fragment = new FragmentNavBot();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
