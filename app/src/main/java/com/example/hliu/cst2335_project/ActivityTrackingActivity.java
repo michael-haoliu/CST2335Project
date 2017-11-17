7d546571f7c77add4759f21263ae1c7cfbd26f71
cliff <cliff_ok@outlook.com>
2017-11-16 22:22:42 -0500

    Geyan commit

--- app/src/main/java/com/example/hliu/cst2335_project/ActivityTrackingActivity.java
+++ app/src/main/java/com/example/hliu/cst2335_project/ActivityTrackingActivity.java
@@ -1,14 +1,8 @@
 package com.example.hliu.cst2335_project;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -17,9 +11,7 @@ import android.view.MenuItem;
 import com.example.hliu.cst2335_project.ActivityTrackingPkg.ActivityMenuHelp;
 import com.example.hliu.cst2335_project.ActivityTrackingPkg.FragmentNavBot;
 
-public class ActivityTrackingActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-    private NavigationView navigationView;
+public class ActivityTrackingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +21,6 @@ public class ActivityTrackingActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
         FragmentNavBot fragment = new FragmentNavBot();
         android.support.v4.app.FragmentTransaction fragmentTransaction =
@@ -45,16 +29,6 @@ public class ActivityTrackingActivity extends AppCompatActivity
         fragmentTransaction.commit();
     }
 
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -84,25 +58,5 @@ public class ActivityTrackingActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_activity) {
-            FragmentNavBot fragment = new FragmentNavBot();
-            android.support.v4.app.FragmentTransaction fragmentTransaction =
-                    getSupportFragmentManager().beginTransaction();
-            fragmentTransaction.replace(R.id.fragment_container, fragment);
-            fragmentTransaction.commit();
-        }
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
